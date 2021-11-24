83fe87ce568317782827bf5f6c79b04b7bd6af49
Alexey Gritskevich <agritskevichx@gmail.com>
2021-11-24 15:29:56 +0600

    add get all, get one and create endpoints for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService) {}
+
+    @Post()
+    create(@Body() userDto: CreateUserDto) {
+        return this.usersService.createUser(userDto);
+    }
+
+    @Get()
+    getAll() {
+        return this.usersService.getAllUsers();
+    }
+
+    @Get(':id')
+    getOne(@Param('id') id: string) {
+        return this.usersService.getUserById(+id);
+    }
+}
