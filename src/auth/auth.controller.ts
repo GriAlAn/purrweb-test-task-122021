cf91e6b47593b1ef02dc1e263bd9a546250e81e0
Alexey Gritskevich <agritskevichx@gmail.com>
2021-11-25 10:36:12 +0600

    add documentation for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 
+@ApiTags('Authorization')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {
   }
 
+  @ApiOperation({summary: 'Login (sign in)'})
+  @ApiResponse({status: 200, description: 'Success'})
   @Post('/login')
   login(@Body() userDto: CreateUserDto) {
     return this.authService.login(userDto);
   }
 
+  @ApiOperation({summary: 'Registration (sign up)'})
+  @ApiResponse({status: 200, description: 'Success'})
   @Post('/registration')
   registration(@Body() userDto: CreateUserDto) {
     return this.authService.registration(userDto);
